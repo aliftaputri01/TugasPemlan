0836d5cef0ce3e30e25d6e167915b4f9dbba5e45
aliftaputri01 <aliftapr@gmail.com>
2020-09-23 18:35:06 +0700

    pertama commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	printf("\tBiodata Pribadi\n\n");
+	printf("Nama               :Alifta Putri Ramadhani\n");
+	printf("Alamat             :Jl Kalasan 44\n");
+	printf("Prodi              :Teknik Informatika\n");
+	printf("Npm                :19081010129\n");
+	
+	getch();
+	
+	return 0;
+}
